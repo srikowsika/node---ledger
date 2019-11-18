db50be185c570378c5ca438b57c07232adfc3790
srikowsika <srikowsika.guna@aspiresys.com>
2019-11-18 19:37:14 +0530

    node-ledger

--- app.js
+++ app.js
@@ -1,92 +1,123 @@
 
-const express=require('express');
-const app= express();
-var replace = require("replace-in-file");
-var prependFile = require('prepend-file');
-const cors=require('cors');
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const fs = require("fs");
 const Ledger = require('ledger-cli').Ledger;
-const bodyParser = require('body-parser');
-const loginFormRoute=require('./router/ledgerform')
-app.use(cors());
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
-app.use('/',loginFormRoute)
-var ledger = new Ledger({ file: 'Journal.dat',binary:'/usr/bin/ledger' });
+var replace = require("replace-in-file");
+var prependFile = require('prepend-file');
+var transId = require('autoincrement');
 
-app.post('/',(req,res)=>{
+const loginFormRoute = require('./router/ledgerform')
+
+app.use(cors());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
+app.use('/', loginFormRoute)
+
+var ledger = new Ledger({ file: 'Journal.dat', binary: '/usr/bin/ledger' });
+
+function objectTOString(entry)
+{
+  var payee;
+    var date;
+    entry.effectiveDate === null ? date = `${entry.date}` : date = `${entry.date}=${entry.effectiveDate}]`;
+    entry.status === 'cleared' ? payee = `* ${entry.payee}` : payee = `${entry.payee}`;
+    entry.pending === 'pending'? payee = `! ${entry.payee}` : payee = `${entry.payee}`;
+    transId = transId + 1;
+    var code=transId+2;
+    var ledgerString = `${date} (${code}:${transId}) ${payee}\n`
+    for (posting of entry.postings) {
+      if(posting.comments !== null){ledgerString += `  ; ${posting.comments}\n`}
+      ledgerString += `  ${posting.account}           ${posting.amount}\n`
+    }
+    return ledgerString;
+}
+function findEntryToReplace()
+{
+  fs.readFile('Journal.dat', 'utf8', function read(err, data) {
+    if (err) { console.log('Id not exists') }
+    var regexp = /(\d{4}[-|:|\/]\d{2}[-|\/|:]\d{2}).?[(](10:1)[)].*?(^\s*$)/gms
+    console.log(regexp)
+    console.log(typeof (regexp))
+    replaceEntry = data.match(regexp);
+  })
+  return replaceEntry[0]
+}
+
+app.post('/comment', (req, res) => {
   console.log(req.body)
-  
-  comments=req.body.formDetails.comments
-  //console.log(comments)
-    prependFile('Journal.dat', '; '+comments+'\n', function (err) {
-      if (err) {
-          throw err
-      }
-      //console.log('The "data to prepend" was prepended to file!');
+  comments = req.body.formDetails.comments
+  prependFile('/node-ledger/Journal.dat', '; ' + comments + '\n', function (err) {
+    if (err) {
+      throw err
+    }
   });
   res.send('Node')
 })
-var entry={ 
-  date: '2019-12-01',
-  effectiveDate: null,
-  code: '10',
-  cleared: false,
-  pending: false,
-  payee: 'Checking balance',
-  postings:
-   [ { account: 'Expenses:Auto:Gas', commodity:{ currency: '£', amount: 37, formatted: '£ 37.50' }  },
-     { account: 'Assets:Checking', commodity:{ currency: '£', amount: 37.50, formatted: '£ -37.50' }} ] 
-    }
-    var payee;
-    var date=entry.date;
-    entry.cleared===true ? payee=`* ${entry.payee}` : payee=`${entry.payee}`;
-    entry.pending===true ? payee=`! ${entry.payee}`  : payee=`${entry.payee}`;
-    
-    var jsonData = `${date} (${entry.code}) ${payee}\n`
-    for(posting of entry.postings)
-    {
-      jsonData+=`  ${posting.account}           ${posting.commodity.formatted}\n`
-    }
-    
-fs.appendFile("Journal.dat",jsonData+'\n', 'utf-8', function (err) {
+
+app.post('/form', (req, res) => {
+    var entry=req.body.formDetails;
+    console.log(entry)
+    var ledgerString=objectTOString(entry)
+    fs.appendFile("Journal.dat", ledgerString + '\n', 'utf-8', function (err) {
       if (err) {
-          console.log("An error occured while writing JSON Object to File.");
-          return console.log(err);
+        console.log("An error occured while writing JSON Object to File.");
+        return console.log(err);
       }
-      console.log("JSON file has been saved.");
-  });
-
-ledger.register(entry.code===1)
-.on('data', function(entry) {
-  console.log(entry);
-  console.log(entry.postings);
+    });
+  res.send('Node')
 })
-.once('error', function(error) {
-  console.log(error)
-});
 
-fs.readFile('Journal.dat', 'utf8', function read(err, data) {
-  if (err) {throw err;}
-  var regexp=/(\d{4}[-|:|\/]\d{2}[-|\/|:]\d{2}).?[(](5)[)].*?(^\s*$)/gms
-  
-  console.log(regexp)
-  console.log(typeof(regexp))
-  data1= data.match(regexp);
-  console.log(data1)
+app.post('/replace',(req,res)=>{
+  var replaceEntry=findEntryToReplace();
+  var replaceString=objectTOString(req.body.formDetails)
+  console.log(replaceEntry);
+  console.log(replaceString);
   const results = replace.sync({
     files: 'Journal.dat',
-    from: data1[0],
-    to: jsonData,
+    from: replaceEntry,
+    to: replaceString,
     countMatches: true,
   });
   console.log(results);
-  })  
+})
+
+ledger.register()
+  .on('data', function (entry) {
+    console.log(entry);
+  })
+  .once('error', function (error) {
+    console.log(error)
+  });
 
 ledger.accounts()
 .on('data', function(entry) {
   console.log(entry)
 })
 
-  
 module.exports = app;
+
+
+
+
+
+// fs.readFile('Journal.dat', 'utf8', function read(err, data) {
+//   if (err) { console.log('Id not exists') }
+//   var regexp = /(\d{4}[-|:|\/]\d{2}[-|\/|:]\d{2}).?[(](10:1)[)].*?(^\s*$)/gms
+//   console.log(regexp)
+//   console.log(typeof (regexp))
+//   data1 = data.match(regexp);
+//   console.log(data1)
+//   const results = replace.sync({
+//     files: 'Journal.dat',
+//     from: data1[0],
+//     to: jsonData,
+//     countMatches: true,
+//   });
+//   console.log(results);
+// })
+
